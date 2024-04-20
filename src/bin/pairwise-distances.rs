86384475e14756b2a06d1d05953c0e25445933a8
Trevor <trevor.schneggenburger@gmail.com>
2024-04-20 12:21:41 -0400

    Changed pairwise-distances to use RoaringBitmaps to perform set operations. It appears to be on par with my implementation and definitely uses less memory.

--- src/bin/pairwise-distances.rs
+++ src/bin/pairwise-distances.rs
@@ -1,16 +1,21 @@
 use clap::Parser;
+use itertools::Itertools;
 use log::{debug, info};
 use musk::io::{dump_data_to_file, load_string2taxid};
 use musk::kmer_iter::KmerIter;
-use musk::sorted_vector_utilities::{DifferenceIterator, UnionIterator};
 use musk::utility::get_fasta_iterator_of_file;
+use roaring::{MultiOps, RoaringBitmap};
 use std::collections::{HashMap, HashSet};
 use std::path::Path;
 use std::sync::{mpsc, Arc};
 use threadpool::ThreadPool;
-use musk::sequences::{Sequence, one_to_one, many_to_one, many_to_many, self_matrix};
 
-fn create_bit_vectors(files: &str, kmer_length: usize, taxid: u32) -> Sequence {
+enum Bitmap {
+    One(RoaringBitmap, String, u32),
+    Many(RoaringBitmap, Vec<(RoaringBitmap, String)>, u32),
+}
+
+fn create_bitmaps(files: &str, kmer_length: usize, taxid: u32) -> Bitmap {
     let mut sorted_kmer_vectors = vec![];
     for file in files.split(",") {
         let mut kmer_set = HashSet::new();
@@ -29,45 +34,41 @@ fn create_bit_vectors(files: &str, kmer_length: usize, taxid: u32) -> Sequence {
     }
     if sorted_kmer_vectors.len() == 1 {
         let sequence = sorted_kmer_vectors.into_iter().nth(0).unwrap();
-        Sequence::One(sequence.0, sequence.1, taxid)
+        Bitmap::One(RoaringBitmap::from_sorted_iter(sequence.0).unwrap(), sequence.1, taxid)
     } else {
-        let union = UnionIterator::from(
+        let bitmaps = 
             sorted_kmer_vectors
-                .iter()
-                .map(|vector| &vector.0)
-                .collect::<Vec<&Vec<u32>>>(),
-        )
-        .map(|kmer| *kmer)
-        .collect::<Vec<u32>>();
-        let difference_vectors = sorted_kmer_vectors
+                .into_iter()
+                .map(|(vector, files)| (RoaringBitmap::from_sorted_iter(vector).unwrap(), files))
+                .collect_vec();
+        let union = bitmaps.iter().map(|x| &x.0).union();
+        let difference_vectors = bitmaps 
             .into_iter()
-            .map(|(sorted_kmer_vector, file)| {
+            .map(|(bitmap, file)| {
                 (
-                    DifferenceIterator::from(&union, vec![&sorted_kmer_vector])
-                        .map(|kmer| *kmer)
-                        .collect::<Vec<u32>>(),
+                    &union - bitmap,
                     file,
                 )
             })
-            .collect::<Vec<(Vec<u32>, String)>>();
-        Sequence::Many(union, difference_vectors, taxid)
+            .collect_vec();
+        Bitmap::Many(union, difference_vectors, taxid)
     }
 }
 
 fn create_file_to_index_map(
-    sequences: &Vec<Sequence>,
+    sequences: &Vec<Bitmap>,
 ) -> (Vec<(String, u32)>, HashMap<String, usize>) {
     let files = sequences
         .into_iter()
         .map(|sequence| match sequence {
-            Sequence::One(_set, file, taxid) => vec![(file.clone(), *taxid)],
-            Sequence::Many(_union, files, taxid) => files
+            Bitmap::One(_set, file, taxid) => vec![(file.clone(), *taxid)],
+            Bitmap::Many(_union, files, taxid) => files
                 .into_iter()
                 .map(|(_difference, file)| (file.clone(), *taxid))
-                .collect::<Vec<(String, u32)>>(),
+                .collect_vec(),
         })
         .flatten()
-        .collect::<Vec<(String, u32)>>();
+        .collect_vec();
     let map = HashMap::from_iter(
         files
             .iter()
@@ -75,6 +76,10 @@ fn create_file_to_index_map(
             .map(|(index, (file, _taxid))| (file.clone(), index)),
     );
     (files, map)
+}
+
+fn distance(size_1: u64, size_2: u64, intersection_size: u64) -> u32 {
+    (size_1 + size_2 - (2 * intersection_size)) as u32
 }
 
 /// Creates a file to tax id mapping where files with the same tax id are grouped
@@ -117,7 +122,7 @@ fn main() {
     for (files, taxid) in file2taxid {
         let sender_clone = sender.clone();
         pool.execute(move || {
-            let sequence = create_bit_vectors(&*files, args.kmer_length, taxid);
+            let sequence = create_bitmaps(&*files, args.kmer_length, taxid);
             sender_clone.send(sequence).unwrap();
         })
     }
@@ -126,79 +131,96 @@ fn main() {
         sequences.push(triple);
     }
 
-    info!("sorted kmer vectors computed, computing pairwise distances...");
-
     let (index_to_file_and_taxid, file_to_index) = create_file_to_index_map(&sequences);
+
+    info!("sorted kmer vectors computed, computing pairwise distances...");
 
     let mut all_distances = index_to_file_and_taxid
         .into_iter()
         .map(|(file, taxid)| (vec![0_u32; file_to_index.len()], file, taxid))
-        .collect::<Vec<(Vec<u32>, String, u32)>>();
+        .collect_vec();
     let (sender, receiver) = mpsc::channel();
     let sequences_arc = Arc::new(sequences);
-    let pool_arc = Arc::new(pool);
     let file_to_index_arc = Arc::new(file_to_index);
-    for index_1 in 0..sequences_arc.len() {
+    for sequence_index_1 in 0..sequences_arc.len() {
         let sender_clone = sender.clone();
         let sequences_arc_clone = sequences_arc.clone();
         let file_to_index_arc_clone = file_to_index_arc.clone();
-        pool_arc.execute(move || {
-            for index_2 in 0..sequences_arc_clone.len() {
-                if index_2 < index_1 {
+        pool.execute(move || {
+            for sequence_index_2 in 0..sequences_arc_clone.len() {
+                if sequence_index_2 < sequence_index_1 {
                     continue;
-                } else if index_2 == index_1 {
-                    if let Sequence::Many(union, differences, _) = &sequences_arc_clone[index_1] {
-                        self_matrix(
-                            (union, differences),
-                            &sender_clone,
-                            &file_to_index_arc_clone,
-                        )
+                } else if sequence_index_2 == sequence_index_1 {
+                    if let Bitmap::Many(union, differences, _) = &sequences_arc_clone[sequence_index_1] {
+                        for difference_index_1 in 0..differences.len() {
+                            for difference_index_2 in 0..differences.len() {
+                                if difference_index_2 <= difference_index_1 {
+                                    continue;
+                                }
+                                let (difference_1, difference_2) = (&differences[difference_index_1], &differences[difference_index_2]);
+                                let intersection_size = union.difference_len(&vec![&difference_1.0, &difference_2.0].union());
+                                let size_1 = union.len() - difference_1.0.len();
+                                let size_2 = union.len() - difference_2.0.len();
+                                let distance = distance(size_1, size_2, intersection_size);
+                                let (send_index_1, send_index_2) = (*file_to_index_arc_clone.get(&difference_1.1).unwrap(), *file_to_index_arc_clone.get(&difference_2.1).unwrap());
+                                sender_clone.send((send_index_1, send_index_2, distance)).unwrap();
+                            }
+                        }
                     }
                 } else {
-                    match (&sequences_arc_clone[index_1], &sequences_arc_clone[index_2]) {
+                    match (&sequences_arc_clone[sequence_index_1], &sequences_arc_clone[sequence_index_2]) {
                         (
-                            Sequence::Many(union_1, differences_1, _taxid_1),
-                            Sequence::Many(union_2, differences_2, _taxid_2),
+                            Bitmap::Many(union_1, differences_1, _taxid_1),
+                            Bitmap::Many(union_2, differences_2, _taxid_2),
                         ) => {
-                            many_to_many(
-                                (union_1, differences_1),
-                                (union_2, differences_2),
-                                &sender_clone,
-                                &file_to_index_arc_clone,
-                            );
+                            let union_intersection = vec![union_1, union_2].intersection();
+                            for difference_1 in differences_1 {
+                                for difference_2 in differences_2 {
+                                    let intersection_size = union_intersection.difference_len(&vec![&difference_1.0, &difference_2.0].union());
+                                    let size_1 = union_1.len() - difference_1.0.len();
+                                    let size_2 = union_2.len() - difference_2.0.len();
+                                    let distance = distance(size_1, size_2, intersection_size);
+                                    let (send_index_1, send_index_2) = (*file_to_index_arc_clone.get(&difference_1.1).unwrap(), *file_to_index_arc_clone.get(&difference_2.1).unwrap());
+                                    sender_clone.send((send_index_1, send_index_2, distance)).unwrap();
+                                }
+                            }
                         }
                         (
-                            Sequence::Many(union, differences, _taxid_1),
-                            Sequence::One(set, file, _taxid_2),
+                            Bitmap::Many(union, differences, _taxid_1),
+                            Bitmap::One(set, file, _taxid_2),
                         ) => {
-                            many_to_one(
-                                (union, differences),
-                                (set, file),
-                                &sender_clone,
-                                &file_to_index_arc_clone,
-                            );
+                            let intersection = vec![union, set].intersection();
+                            let size_1 = set.len();
+                            for difference in differences {
+                                let intersection_size = intersection.difference_len(&difference.0);
+                                let size_2 = union.len() - difference.0.len();
+                                let distance = distance(size_1, size_2, intersection_size);
+                                let (send_index_1, send_index_2) = (*file_to_index_arc_clone.get(&difference.1).unwrap(), *file_to_index_arc_clone.get(file).unwrap());
+                                sender_clone.send((send_index_1, send_index_2, distance)).unwrap();
+                            }
                         }
                         (
-                            Sequence::One(set, file, _taxid_1),
-                            Sequence::Many(union, differences, _taxid_2),
+                            Bitmap::One(set, file, _taxid_1),
+                            Bitmap::Many(union, differences, _taxid_2),
                         ) => {
-                            many_to_one(
-                                (union, differences),
-                                (set, file),
-                                &sender_clone,
-                                &file_to_index_arc_clone,
-                            );
+                            let intersection = vec![union, set].intersection();
+                            let size_1 = set.len();
+                            for difference in differences {
+                                let intersection_size = intersection.difference_len(&difference.0);
+                                let size_2 = union.len() - difference.0.len();
+                                let distance = distance(size_1, size_2, intersection_size);
+                                let (send_index_1, send_index_2) = (*file_to_index_arc_clone.get(&difference.1).unwrap(), *file_to_index_arc_clone.get(file).unwrap());
+                                sender_clone.send((send_index_1, send_index_2, distance)).unwrap();
+                            }
                         }
                         (
-                            Sequence::One(set_1, file_1, _taxid_1),
-                            Sequence::One(set_2, file_2, _taxid_2),
+                            Bitmap::One(set_1, file_1, _taxid_1),
+                            Bitmap::One(set_2, file_2, _taxid_2),
                         ) => {
-                            one_to_one(
-                                (set_1, file_1),
-                                (set_2, file_2),
-                                &sender_clone,
-                                &file_to_index_arc_clone,
-                            );
+                            let intersection_size = set_1.intersection_len(set_2);
+                            let distance = distance(set_1.len(), set_2.len(), intersection_size);
+                            let (send_index_1, send_index_2) = (*file_to_index_arc_clone.get(file_1).unwrap(), *file_to_index_arc_clone.get(file_2).unwrap());
+                            sender_clone.send((send_index_1, send_index_2, distance)).unwrap();
                         }
                     }
                 }
