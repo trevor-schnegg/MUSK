37456085f9f50cb5b8ee5a3b4bc4813e2bb5c378
Trevor <trevor.schneggenburger@gmail.com>
2024-05-24 13:49:25 -0400

    Repurposed the subset test binary for Sam

--- src/bin/run-subset-test.rs
+++ src/bin/run-subset-test.rs
@@ -1,17 +1,7 @@
 use clap::Parser;
-use log::{debug, info};
-use musk::{
-    io::load_data_from_file,
-    rle::{BuildRunLengthEncoding, RunLengthEncoding},
-};
-use rand::{seq::SliceRandom, thread_rng};
-use rayon::prelude::*;
-use roaring::RoaringBitmap;
-use statrs::statistics::Statistics;
-use std::{
-    collections::{HashMap, HashSet},
-    path::Path,
-};
+use itertools::Itertools;
+use musk::{io::load_data_from_file, rle::{Run, RunLengthEncoding}};
+use std::path::Path;
 
 /// Creates a sample of k-mers from the matrix
 #[derive(Parser)]
@@ -19,8 +9,8 @@ use std::{
 #[clap(author = "Trevor S. <trevor.schneggenburger@gmail.com>")]
 struct Args {
     #[arg()]
-    /// Location to output the serialzed bitmaps
-    subset_bitmaps: String,
+    /// Location of the subset rle vectors
+    subset_rle: String,
 }
 
 fn main() {
@@ -28,60 +18,10 @@ fn main() {
 
     // Parse arguments from the command line
     let args = Args::parse();
-    let subset_bitmaps_path = Path::new(&args.subset_bitmaps);
+    let subset_rles_path = Path::new(&args.subset_rle);
 
-    let (subset_kmers, subset_bitmaps) = load_data_from_file::<(
-        HashSet<u32>,
-        Vec<(String, RoaringBitmap, u32)>,
-    )>(subset_bitmaps_path);
+    let subset_rles = load_data_from_file::<Vec<(u32, RunLengthEncoding)>>(subset_rles_path);
 
-    info!("testing rle...");
-    let mut rle_map: HashMap<u32, BuildRunLengthEncoding> = HashMap::from_iter(
-        subset_kmers
-            .iter()
-            .map(|kmer| (*kmer, BuildRunLengthEncoding::new())),
-    );
-    for (index, (_files, bitmap, _taxid)) in subset_bitmaps.iter().enumerate() {
-        for kmer in bitmap {
-            rle_map.get_mut(&kmer).unwrap().push(index);
-        }
-    }
-    let compressed_database = rle_map
-        .into_par_iter()
-        .map(|(_kmer, build_rle)| build_rle.to_rle())
-        .collect::<Vec<RunLengthEncoding>>();
-    let compressed_rle_length = bincode::serialize(&compressed_database).unwrap().len();
-    info!("compressed rle length {}", compressed_rle_length);
-
-    info!("testing permutations of rle...");
-    let mut rng = thread_rng();
-    let mut rle_lengths = vec![];
-    for i in 0..100 {
-        let mut new_permutation = subset_bitmaps.clone();
-        new_permutation.shuffle(&mut rng);
-        let mut rle_map: HashMap<u32, BuildRunLengthEncoding> = HashMap::from_iter(
-            subset_kmers
-                .iter()
-                .map(|kmer| (*kmer, BuildRunLengthEncoding::new())),
-        );
-        for (index, (_files, bitmap, _taxid)) in new_permutation.iter().enumerate() {
-            for kmer in bitmap {
-                rle_map.get_mut(&kmer).unwrap().push(index);
-            }
-        }
-        let compressed_database = rle_map
-            .into_par_iter()
-            .map(|(_kmer, build_rle)| build_rle.to_rle())
-            .collect::<Vec<RunLengthEncoding>>();
-        let compressed_rle_length = bincode::serialize(&compressed_database).unwrap().len();
-        rle_lengths.push(compressed_rle_length);
-        if i % 10 == 0 && i != 0 {
-            debug!("completed {} permutations", i);
-        }
-    }
-    info!(
-        "average rle length {}, std dev rle length {}",
-        rle_lengths.iter().map(|x| *x as f64).mean(),
-        rle_lengths.iter().map(|x| *x as f64).std_dev()
-    );
+    println!("{:?}", subset_rles[0].1.get_vector());
+    println!("{:?}", subset_rles[0].1.get_vector().into_iter().map(|x| Run::from_u16(*x)).collect_vec());
 }
