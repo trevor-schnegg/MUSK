34b1a74df5f625d24172d1259f5f0d38db4ec39f
Trevor <trevor.schneggenburger@gmail.com>
2024-06-03 23:41:37 -0400

    Finished with rle runs iterator. playground.rs shows that it appears to be functioning correctly.

--- src/bin/playground.rs
+++ src/bin/playground.rs
@@ -1,23 +1,23 @@
-use musk::kmer_iter::KmerIter;
+use musk::{kmer_iter::KmerIter, rle::{Run, RunLengthEncodingIter}};
 
 use itertools::Itertools;
-use musk::rle::{BuildRunLengthEncoding, Run};
+use musk::rle::BuildRunLengthEncoding;
 
-const CLEAR_BITS: usize = 2_usize.pow((14 * 2) as u32) - 1;
+// const CLEAR_BITS: usize = 2_usize.pow((14 * 2) as u32) - 1;
 
-fn reverse_compliment(kmer: usize) -> usize {
-    let mut buffer = 0;
-    let mut complement_kmer = (!kmer) & CLEAR_BITS;
-    for _ in 0..14 {
-        // Pop the right-most letter
-        let letter = complement_kmer & 3;
-        complement_kmer >>= 2;
-        // Add to the right of the buffer
-        buffer <<= 2;
-        buffer |= letter;
-    }
-    buffer
-}
+// fn reverse_compliment(kmer: usize) -> usize {
+//     let mut buffer = 0;
+//     let mut complement_kmer = (!kmer) & CLEAR_BITS;
+//     for _ in 0..14 {
+//         // Pop the right-most letter
+//         let letter = complement_kmer & 3;
+//         complement_kmer >>= 2;
+//         // Add to the right of the buffer
+//         buffer <<= 2;
+//         buffer |= letter;
+//     }
+//     buffer
+// }
 
 fn main() {
     let seq = "ATGCTGA".as_bytes();
@@ -33,35 +33,24 @@ fn main() {
 
     let _maximum = (1_usize << 14) - 1;
 
-    let vector = vec![5, 6, 7, 8, 9, 20, 25];
+    let mut dense_vector = (5_usize..50_usize).into_iter().collect_vec();
+    let added_vec: Vec<usize> = vec![55, 58, 60, 64, 65, 66, 80, 120];
+    for n in added_vec {
+        dense_vector.push(n);
+    }
 
     let mut build_rle_1 = BuildRunLengthEncoding::new();
-    for int in vector {
-        build_rle_1.push(int);
+    for int in &dense_vector {
+        build_rle_1.push(*int);
     }
     let rle_1 = build_rle_1.to_rle();
-    println!(
-        "{:?}",
-        rle_1
-            .get_vector()
-            .iter()
-            .map(|x| Run::from_u16(*x))
-            .collect_vec()
-    );
-    println!(
-        "{:?}",
-        rle_1
-            .get_vector()
-            .iter()
-            .map(|x| Run::from_u16(*x))
-            .collect_vec()
-    );
 
-    let mut palendrome_count = 0_usize;
-    for i in 0..4_usize.pow(14) {
-        if i == reverse_compliment(i) {
-            palendrome_count += 1;
-        }
+    println!("{:?}", rle_1.get_vector().into_iter().map(|x| Run::from_u16(*x)).collect_vec());
+    
+    let ground_truth = dense_vector.into_iter();
+    let test_rle = RunLengthEncodingIter::from_runs_vector(rle_1.get_vector());
+    for (x, y) in ground_truth.zip(test_rle) {
+        println!("{}, {}", x, y);
+        assert_eq!(x, y);
     }
-    println!("palendromes for 14-mers: {}", palendrome_count);
 }
