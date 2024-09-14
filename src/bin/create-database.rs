21c043d44eae78e220e2b5cf21e8ea52c29c1e83
Trevor <trevor.schneggenburger@gmail.com>
2024-09-14 15:23:14 -0400

    removed a debug feature

--- src/bin/create-database.rs
+++ src/bin/create-database.rs
@@ -8,8 +8,7 @@ use musk::utility::create_bitmap;
 use rayon::prelude::*;
 use roaring::RoaringBitmap;
 use std::path::Path;
-use std::process::exit;
-use tracing::{debug, info};
+use tracing::info;
 
 const CANONICAL: bool = true;
 
@@ -55,10 +54,6 @@ fn main() {
     // Load the file2taxid ordering
     info!("loading file2taxid at {}", args.file2taxid);
     let file2taxid_ordering = load_string2taxid(file2taxid_path);
-
-    debug!("{:?}", file2taxid_ordering);
-
-    exit(1);
 
     info!("creating roaring bitmaps for each group...");
     let bitmaps = file2taxid_ordering
