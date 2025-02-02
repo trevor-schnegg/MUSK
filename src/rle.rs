f7376cd76797b428de65fd94397978aa9f019ced
Trevor <trevor.schneggenburger@gmail.com>
2025-02-01 19:46:49 -0500

    Refactored rle construction

--- src/rle.rs
+++ src/rle.rs
@@ -128,7 +128,7 @@ impl NaiveRunLengthEncoding {
     }
 
     pub fn to_rle(self) -> RunLengthEncoding {
-        RunLengthEncoding::compress_from(self.runs)
+        RunLengthEncoding::allow_uncompressed_from(self.runs)
     }
 }
 
@@ -153,56 +153,71 @@ impl RunLengthEncoding {
         RunLengthEncoding { blocks }
     }
 
-    fn compress_from(runs: Vec<u16>) -> Self {
+    fn allow_uncompressed_from(runs: Vec<u16>) -> Self {
         // The compressed vector that composes the new run length encoding
-        let mut compressed_blocks = vec![];
+        let mut blocks_w_uncompressed_allowed = Vec::with_capacity(runs.len());
 
-        // A buffer that represents a bit set of at most MAX_UNCOMPRESSED_BITS
+        // A buffer that represents a bit map of at most MAX_UNCOMPRESSED_BITS
         let mut bits_buffer = vec![];
 
         // A variable to store the current number of bits in the buffer
         // This is NOT equal to the length of the buffer
         let mut num_bits_in_buffer = 0_usize;
 
-        for run in runs.into_iter().map(|x| Block::from_u16(x)) {
+        runs.into_iter().for_each(|naive_u16| {
+            let naive_block = Block::from_u16(naive_u16);
+
             // Get the number of bits in the current run
-            let run_bits = match run {
+            let naive_block_bits = match naive_block {
                 Block::Ones(count) => count,
                 Block::Zeros(count) => count,
-                Block::Uncompressed(_) => panic!("tried to call compress on an rle twice"),
+                Block::Uncompressed(_) => panic!("tried to allow uncompressed blocks in an RLE that already has uncompressed blocks"),
             } as usize;
 
-            if num_bits_in_buffer + run_bits < MAX_UNCOMPRESSED_BITS {
+            if num_bits_in_buffer + naive_block_bits < MAX_UNCOMPRESSED_BITS {
                 // If the run can be stored in the buffer, simply push it to the buffer
-                bits_buffer.push(run);
-                num_bits_in_buffer += run_bits;
-            } else if num_bits_in_buffer + run_bits == MAX_UNCOMPRESSED_BITS {
+                bits_buffer.push(naive_block);
+                num_bits_in_buffer += naive_block_bits;
+
+            } else if num_bits_in_buffer + naive_block_bits == MAX_UNCOMPRESSED_BITS {
                 // If this run is added the buffer will be exactly at capacity
 
-                // If the buffer started as empty, there is no advantage (or disadvantage) to storing this run as uncompressed
-                // Therefore, this is done for code simplicity and nothing else
-                bits_buffer.push(run);
-                let decompressed_run = decompress_buffer(&mut bits_buffer);
+                if bits_buffer.is_empty() {
+                    // If the bits buffer is empty, then a single run exactly filled the buffer
+                    // Therefore, it does not matter if we store as a run or as uncompressed
+                    // Simply push the original u16 to the new vector
+                    blocks_w_uncompressed_allowed.push(naive_u16);
 
-                assert!(bits_buffer.is_empty());
-                num_bits_in_buffer = 0;
+                    // Assert that we've maintained our invariant
+                    assert_eq!(num_bits_in_buffer, 0);
+                } else {
+                    // Otherwise, there was something already in the buffer
+                    bits_buffer.push(naive_block);
 
-                compressed_blocks.push(decompressed_run);
+                    // Now at least 2 items are in the buffer, we can save by using uncompressed
+                    blocks_w_uncompressed_allowed.push(create_uncompressed_from(&bits_buffer));
+
+                    // Clear the bits buffer and the number of bits in it
+                    bits_buffer.clear(); num_bits_in_buffer = 0;
+                }
             } else {
                 // Adding the next run would be strictly greater than MAX_UNCOMPRESSED_BITS
                 if bits_buffer.is_empty() {
                     // If the buffer is empty, this run overhangs MAX_UNCOMPRESSED_BITS
-                    // Simply push it to the compressed vector
-                    compressed_blocks.push(run);
+                    // Simply push the original u16 to the new vector
+                    blocks_w_uncompressed_allowed.push(naive_u16);
+
+                    // Assert that we've maintained our invariant
+                    assert_eq!(num_bits_in_buffer, 0);
                 } else {
                     // The buffer is not empty AND adding the next run overhangs MAX_UNCOMPRESSED_BITS
                     // Therefore, borrow some bits from the run we are trying to add to the buffer to fill it
                     let num_bits_to_fill_buffer =
                         (MAX_UNCOMPRESSED_BITS - num_bits_in_buffer) as u16;
-                    let leftover_num_bits = run_bits as u16 - num_bits_to_fill_buffer;
+                    let leftover_num_bits = naive_block_bits as u16 - num_bits_to_fill_buffer;
 
-                    // Split the run into one to fill the buffer and one leftover
-                    let (buffer_run_to_add, leftover_run) = if let Block::Ones(_) = run {
+                    // Split into 2 blocks
+                    let (block_to_add_to_buffer, leftover_block) = if let Block::Ones(_) = naive_block {
                         (
                             Block::Ones(num_bits_to_fill_buffer),
                             Block::Ones(leftover_num_bits),
@@ -214,60 +229,61 @@ impl RunLengthEncoding {
                         )
                     };
 
-                    bits_buffer.push(buffer_run_to_add);
-                    let decompressed_run = decompress_buffer(&mut bits_buffer);
+                    // Push the new block to fill the buffer
+                    bits_buffer.push(block_to_add_to_buffer);
 
-                    assert!(bits_buffer.is_empty());
-                    num_bits_in_buffer = 0;
+                    // Create an uncompressed block from the buffer and push to the new vector
+                    blocks_w_uncompressed_allowed.push(create_uncompressed_from(&bits_buffer));
 
-                    // Push the decompressed bits to the compressed vector
-                    compressed_blocks.push(decompressed_run);
+                    // Clear the bits buffer and the number of bits in it
+                    bits_buffer.clear(); num_bits_in_buffer = 0;
 
                     // Handle the leftover bits
                     if leftover_num_bits < MAX_UNCOMPRESSED_BITS as u16 {
-                        bits_buffer.push(leftover_run);
+                        // If the run can be stored in the buffer, simply push it to the buffer
+                        bits_buffer.push(leftover_block);
                         num_bits_in_buffer += leftover_num_bits as usize;
                     } else {
-                        compressed_blocks.push(leftover_run);
+                        // Otherwise, push the run as a u16 to the new vector
+                        blocks_w_uncompressed_allowed.push(leftover_block.to_u16());
                     }
                 }
             }
-        }
+        });
 
         // After the last run is processed, handle the leftover bits in the buffer (if any)
         if bits_buffer.len() <= 1 {
-            compressed_blocks.append(&mut bits_buffer);
+            bits_buffer
+                .into_iter()
+                .for_each(|run| blocks_w_uncompressed_allowed.push(run.to_u16()));
         } else {
-            compressed_blocks.push(decompress_buffer(&mut bits_buffer))
+            blocks_w_uncompressed_allowed.push(create_uncompressed_from(&bits_buffer))
         }
 
-        let blocks = compressed_blocks
-            .into_iter()
-            .map(|run| run.to_u16())
-            .collect::<Box<[u16]>>();
-
-        RunLengthEncoding { blocks }
+        RunLengthEncoding {
+            blocks: blocks_w_uncompressed_allowed.into_boxed_slice(),
+        }
     }
 }
 
 // Takes a buffer of exactly MAX_UNCOMPRESSED_BITS and converts it to a bit set
-fn decompress_buffer(buffer: &mut Vec<Block>) -> Block {
-    let mut decompressed = 0;
+fn create_uncompressed_from(buffer: &Vec<Block>) -> u16 {
+    let mut uncompressed = 0;
     let mut current_index = 0;
-    for run in buffer.iter() {
-        match *run {
-            Block::Uncompressed(_) => panic!("impossible case reached"),
-            Block::Ones(count) => {
-                for i in current_index..current_index + count {
-                    decompressed |= 1 << i;
-                }
-                current_index += count;
+    buffer.iter().for_each(|run| match *run {
+        Block::Uncompressed(_) => panic!("impossible case"),
+        Block::Ones(count) => {
+            assert!((current_index + count - 1) as usize <= MAX_UNCOMPRESSED_BITS);
+            for i in current_index..current_index + count {
+                uncompressed |= 1 << i;
             }
-            Block::Zeros(count) => current_index += count,
+            current_index += count;
         }
-    }
-    buffer.clear();
-    Block::Uncompressed(decompressed)
+        Block::Zeros(count) => current_index += count,
+    });
+
+    // Return the uncompressed block
+    Block::Uncompressed(uncompressed).to_u16()
 }
 
 enum CurrBlockIter {
